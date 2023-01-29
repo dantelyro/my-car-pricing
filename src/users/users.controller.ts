d9e87c273ad53b3798456e36322649fbfa12389c
Dante <beyondmind9@gmail.com>
2023-01-29 19:34:05 -0300

    added typeOrm

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from './user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: User) {
+    console.log(body);
+  }
+}
