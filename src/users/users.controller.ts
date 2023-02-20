9093dfa63f48206e32d3224909b311db3bedf2c1
Dante <beyondmind9@gmail.com>
2023-02-19 22:06:55 -0300

    finalização do user module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { User } from './user.dto';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Query,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { User } from './dto/user.dto';
+import { UpdateUser } from './dto/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: User) {
-    console.log(body);
+    this.userService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUser) {
+    return this.userService.update(parseInt(id), body);
   }
 }
